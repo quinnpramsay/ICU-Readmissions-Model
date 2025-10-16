281e15471227ec5b6fcb70f6a3fb6ac32255dfe1
quinnpramsay <qramsay@highpoint.edu>
2025-10-16 12:33:58 -0400

    Update password variable for dataset download

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
 pip install MIMIC_IV_MEDS==0.0.6
 export DATASET_DOWNLOAD_USERNAME=$qramsay
-export DATASET_DOWNLOAD_PASSWORD=$49Gulick!
+export DATASET_DOWNLOAD_PASSWORD=$password
 export ROOT_OUTPUT_DIR=path/to/your/desired/directory
